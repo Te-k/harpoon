e039574e4f1a62303dc44265ca6193c3c59dc8e3
Tek <tek@randhome.io>
2017-11-07 23:12:19 -0500

    Add Shodan

---
+++ harpoon/commands/shodancmd.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import sys
+import shodan
+import json
+from harpoon.commands.base import Command
+
+class CommandShodan(Command):
+    name = "shodan"
+    description = "Shodan plugin"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--ip', '-i', help='Check IP of an host')
+        parser.add_argument('--search', '-s', help='Search in shodan')
+        self.parser = parser
+
+    def run(self, conf, args):
+        if 'Shodan' not in conf and 'key' not in conf['Shodan']:
+            print('Bad configuration for Shodan, quitting...')
+            sys.exit(1)
+        api = shodan.Shodan(conf['Shodan']['key'])
+        if args.ip:
+            res = api.host(args.ip)
+            print(json.dumps(res, sort_keys=True, indent=4))
+        elif args.search:
+            res = api.search(args.search)
+            print('%i results' % res['total'])
+            for r in res['matches']:
+                print('[+] %s (%s): port %s/%i -> %s\n' % (
+                        r['ip_str'],
+                        r['org'],
+                        r['transport'],
+                        r['port'],
+                        r['data'][:1000]
+                    )
+                )
+
+        else:
+            self.parser.print_help()
+
