4530403320103d9ccf46133932975d1d417c50cd
Tek <tek@randhome.io>
2017-11-16 17:50:58 -0500

    improve censys

--- harpoon/commands/censyscmd.py
+++ harpoon/commands/censyscmd.py
@@ -2,7 +2,7 @@
 import sys
 import json
 import censys
-from censys import ipv4, certificates
+from censys import ipv4, certificates, query
 from harpoon.commands.base import Command
 
 
@@ -13,47 +13,55 @@ class CommandCensys(Command):
 
     def add_arguments(self, parser):
         subparsers = parser.add_subparsers(help='Subcommand')
-        parser_a = subparsers.add_parser('ip')
+        parser_a = subparsers.add_parser('ip', help='Get information on an IP address')
         parser_a.add_argument('IP', help='IP to be searched')
         parser_a.add_argument('--search', '-s', action='store_true',
                 help='Search for this value in IP infos')
         parser_a.set_defaults(subcommand='ip')
-        parser_a = subparsers.add_parser('cert')
-        parser_a.add_argument('ID', help='ID of the certificate')
-        parser_a.set_defaults(subcommand='cert')
+        parser_b = subparsers.add_parser('cert', help='Get information on a certificate')
+        parser_b.add_argument('ID', help='ID of the certificate')
+        parser_b.set_defaults(subcommand='cert')
+        parser_c = subparsers.add_parser('ipseries', help='list IP series')
+        parser_c.set_defaults(subcommand='ipseries')
         self.parser = parser
 
     def run(self, conf, args):
-        if args.subcommand == 'ip':
-            api = ipv4.CensysIPv4(conf['Censys']['id'], conf['Censys']['secret'])
-            if args.search:
-                res = api.search(args.IP)
-                for r in res:
-                    if len(r['ip']) > 11:
-                        print("[+] %s\t[Location: %s] [Ports: %s]" % (
-                                r['ip'],
-                                r['location.country'],
-                                " ".join(r['protocols'])
+        if 'subcommand' in args:
+            if args.subcommand == 'ip':
+                api = ipv4.CensysIPv4(conf['Censys']['id'], conf['Censys']['secret'])
+                if args.search:
+                    res = api.search(args.IP)
+                    for r in res:
+                        if len(r['ip']) > 11:
+                            print("[+] %s\t[Location: %s] [Ports: %s]" % (
+                                    r['ip'],
+                                    r['location.country'],
+                                    " ".join(r['protocols'])
+                                )
                             )
-                        )
-                    else:
-                        print("[+] %s\t\t[Location: %s] [Ports: %s]" % (
-                                r['ip'],
-                                r['location.country'],
-                                " ".join(r['protocols'])
+                        else:
+                            print("[+] %s\t\t[Location: %s] [Ports: %s]" % (
+                                    r['ip'],
+                                    r['location.country'],
+                                    " ".join(r['protocols'])
+                                )
                             )
-                        )
+                else:
+                    try:
+                        ip = api.view(args.IP)
+                        print(json.dumps(ip, sort_keys=True, indent=4, separators=(',', ': ')))
+                    except censys.base.CensysNotFoundException:
+                        print('IP not found')
+            elif args.subcommand == 'cert':
+                c = certificates.CensysCertificates(conf['Censys']['id'], conf['Censys']['secret'])
+                res = c.view(args.ID)
+                print(json.dumps(res, sort_keys=True, indent=4, separators=(',', ': ')))
+            elif args.subcommand == 'ipseries':
+                cc = query.CensysQuery(api_id=conf['Censys']['id'], api_secret=conf['Censys']['secret'])
+                series = cc.get_series_details("ipv4")
+                for s in series:
+                    print(s)
             else:
-                try:
-                    ip = api.view(args.IP)
-                    print(json.dumps(ip, sort_keys=True, indent=4, separators=(',', ': ')))
-                except censys.base.CensysNotFoundException:
-                    print('IP not found')
-        elif args.subcommand == 'cert':
-            c = certificates.CensysCertificates(conf['Censys']['id'], conf['Censys']['secret'])
-            res = c.view(args.ID)
-            print(json.dumps(res, sort_keys=True, indent=4, separators=(',', ': ')))
-
-
+                self.parser.print_help()
         else:
             self.parser.print_help()
