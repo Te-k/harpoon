debed742fbe9eff263c524046ca1040f9ba393c3
Tek <tek@randhome.io>
2017-12-18 23:07:25 -0500

    Improve ASn command

--- harpoon/commands/asn.py
+++ harpoon/commands/asn.py
@@ -13,25 +13,33 @@ class CommandAsn(Command):
     def add_arguments(self, parser):
         subparsers = parser.add_subparsers(help='Subcommand')
         parser_a = subparsers.add_parser('info', help='Information on an ASn number')
-        parser_a.add_argument('ASN', help='ASN Number', type=int)
+        parser_a.add_argument('ASN', help='ASN Number')
         parser_a.add_argument('--json', '-j', help='Show raw json', action='store_true')
         parser_a.set_defaults(subcommand='info')
         self.parser = parser
 
     def run(self, conf, args, plugins):
+        if args.ASN.lower().startswith("asn"):
+            asn = int(args.ASN[3:])
+        elif args.ASN.lower().startswith("as"):
+            asn = int(args.ASN[2:])
+        else:
+            asn = int(args.ASN)
         if 'subcommand' in args:
             if args.subcommand == 'info':
-                r = requests.get('https://peeringdb.com/api/net?asn=%i' % args.ASN)
+                r = requests.get('https://peeringdb.com/api/net?asn=%i' % asn)
                 if r.status_code == 200:
                     if args.json:
                         print(json.dumps(r.json(), sort_keys=False, indent=4))
                     else:
                         data = r.json()['data'][0]
-                        print('%s' % data['name'])
+                        print('Name: %s' % data['name'])
                         if data['aka'] != '':
-                            print(data['aka'])
+                            print("aka: %s" % data['aka'])
                         if data['notes'] != '':
-                            print(data['notes'])
+                            print("Notes: %s" % data['notes'])
+                        if data['website'] != '':
+                            print("Website: %s" % data['website'])
                         print("Created: %s" % data['created'])
                 else:
                     print("ASN not found")
