68682995f42c16b288a2a6ebc7dd6be076f44c40
jakub <jakub.dalek@utoronto.ca>
2018-01-04 15:37:37 -0500

    harpoon data needs to be copied to site-packages too

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ setup(
         ],
 
     license='GPLv3',
-    packages=['harpoon', 'harpoon.commands', 'harpoon.lib'],
+    packages=['harpoon', 'harpoon.commands', 'harpoon.lib', 'harpoon.data'],
     package_dir={'harpoon.lib': 'harpoon/lib'},
-    package_data={'harpoon': ['harpoon/data']},
+    package_data={'harpoon.data': ['harpoon/data']},
     entry_points= {
         'console_scripts': [ 'harpoon=harpoon.main:main' ]
     }
