f788bc1711333bea2ede7473a95a3c92ec903b99
jakub <jakub.dalek@utoronto.ca>
2018-01-04 15:29:37 -0500

    fixed geolite extraction bug

--- harpoon/commands/ip.py
+++ harpoon/commands/ip.py
@@ -63,10 +63,12 @@ class CommandIp(Command):
         print("-GeoLite2-City.mmdb")
         file_name, headers = urllib.request.urlretrieve('http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz')
         tar = tarfile.open(file_name, 'r')
-        mmdb = tar.extractfile(tar.getmembers()[3])
-        with open(self.geoasn, 'wb+') as f:
-            f.write(mmdb.read())
-        mmdb.close()
+        for this_fn in tar.getmembers():
+            if this_fn.name.endswith("GeoLite2-ASN.mmdb"):
+                mmdb = tar.extractfile(this_fn)
+            with open(self.geoasn, 'wb+') as f:
+                f.write(mmdb.read())
+            mmdb.close()
         print("-GeoLite2-ASN.mmdb")
         print("Download ASN Name database")
         try:
