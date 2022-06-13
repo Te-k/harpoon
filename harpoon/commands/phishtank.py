510673da044cb28df9053685d00944e7c2f07ef6
tek <tek@randhome.io>
2022-06-13 23:48:19 +0200

    Rewrite phishtank plugin

--- harpoon/commands/phishtank.py
+++ harpoon/commands/phishtank.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python
 
-import base64
 import json
-
-import requests
 from harpoon.commands.base import Command
+from harpoon.lib.phishtank import Phishtank, PhishtankError
 
 
 class CommandPhishtank(Command):
@@ -16,53 +14,24 @@ class CommandPhishtank(Command):
     Example:
     ```
     $ harpoon phishtank URL
-    ```  
+    ```
     """
     name = "phishtank"
     description = "Gather information on a URL"
-    config = None 
-    url = "https://checkurl.phishtank.com/checkurl/"
-    headers = {
-            "User-Agent": "Harpoon (https://github.com/Te-k/harpoon)",
-            "Content-Type": "application/x-www-form-urlencoded"
-            }
+    config = {"Phishtank": []}
 
     def add_arguments(self, parser):
-        subparsers = parser.add_subparsers(help='Subcommand')
-        parser_a = subparsers.add_parser('url', help='Information on a url')
-        parser_a.add_argument('URL', help='Phish URL')
-        parser_a.set_defaults(subcommand='url')
-
+        parser.add_argument('URL', help='Phish URL')
         self.parser = parser
 
-    def pretty_print(self, result):
-
-        print("# Phishtank result for:", result['results']['url'])
-        print('------------------------------')
-        if result['results']['in_database'] is True:
-            print('Present in database:', result['results']['in_database'])
-            print('URL to submission:', result['results']['phish_detail_page'])
-            print('Verified:', result['results']['verified'])
-        else:
-            print('Not found in Phishtank database!')
-
-    def run(self, conf, args, plugins):
-        if not 'subcommand' in args:
-                self.parser.print_help()
-        else:
-            app_key = conf['Phishtank']['key']
-            if app_key.strip() != "":
-                app_key = conf['Phishtank']['key']
-            else:
-                app_key = None
-                
-            # Phishtank requires base64 or urlencoded URLs
-            post_data = {
-                    "url": base64.b64encode(args.URL.encode("utf-8")),
-                    "format": "json",
-                    "app_key": app_key,
-                    }
-            r = requests.post(self.url, data=post_data,  headers=self.headers)
-            self.pretty_print(r.json())
-
-
+    def run(self, args, plugins):
+        # May have a configuration
+        print(self._config_data["VirusTotal"].get("toto", "aa"))
+        if "Phishtank" in self._config_data:
+            pt = Phishtank(self._config_data["Phishtank"].get("key", None))
+        try:
+            pt = Phishtank()
+            res = pt.query(args.URL)
+            print(json.dumps(res, indent=4))
+        except PhishtankError:
+            print("Requet failed")
---
+++ harpoon/lib/phishtank.py
@@ -0,0 +1,31 @@
+import base64
+import requests
+
+
+class PhishtankError(Exception):
+    pass
+
+
+class Phishtank:
+    def __init__(self, key=None):
+        self.url = "https://checkurl.phishtank.com/checkurl/"
+        self.headers = {
+            "User-Agent": "Harpoon (https://github.com/Te-k/harpoon)",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        self.key = key
+
+    def query(self, url):
+        post_data = {
+            "url": base64.b64encode(url.encode("utf-8")),
+            "format": "json",
+        }
+        if self.key:
+            post_data["app_key"] = self.key
+        r = requests.post(
+            self.url,
+            data=post_data,
+            headers=self.headers)
+        if r.status_code != 200:
+            raise PhishtankError("Wrong HTTP code {}".format(r.status_code))
+        return r.json()
